90aab6dfdbf581e318a5eeb59ed8a95f54d804e0
Iñigo Gorosabel <inigo.gorosabel@idom.com>
2024-12-09 17:39:00 +0100

    1.1.3 - Propagación de eventos en Swipe

--- projects/osumi-angular-tools/src/lib/directives/swipe.directive.ts
+++ projects/osumi-angular-tools/src/lib/directives/swipe.directive.ts
@@ -2,13 +2,16 @@ import {
   Directive,
   ElementRef,
   inject,
+  OnDestroy,
   output,
   OutputEmitterRef,
+  Renderer2,
 } from '@angular/core';
 import { SwipeData } from '../interfaces/swipe.interface';
 
 @Directive({
   selector: '[oatSwipe]',
+  standalone: true,
   host: {
     '(touchstart)': 'onTouchStart($event)',
     '(touchend)': 'onTouchEnd($event)',
@@ -16,12 +19,41 @@ import { SwipeData } from '../interfaces/swipe.interface';
     '(mouseup)': 'onMouseUp($event)',
   },
 })
-export class SwipeDirective {
+export class SwipeDirective implements OnDestroy {
   private el: ElementRef = inject(ElementRef);
+  private renderer: Renderer2 = inject(Renderer2);
   swipeEnd: OutputEmitterRef<SwipeData> = output<SwipeData>();
 
   private startX: number = 0;
   private startY: number = 0;
+
+  private touchStartListener: () => void;
+  private touchEndListener: () => void;
+  private mouseDownListener: () => void;
+  private mouseUpListener: () => void;
+
+  constructor() {
+    this.touchStartListener = this.renderer.listen(
+      this.el.nativeElement,
+      'touchstart',
+      (event: TouchEvent): void => this.onTouchStart(event)
+    );
+    this.touchEndListener = this.renderer.listen(
+      this.el.nativeElement,
+      'touchend',
+      (event: TouchEvent): void => this.onTouchEnd(event)
+    );
+    this.mouseDownListener = this.renderer.listen(
+      this.el.nativeElement,
+      'mousedown',
+      (event: MouseEvent): void => this.onMouseDown(event)
+    );
+    this.mouseUpListener = this.renderer.listen(
+      this.el.nativeElement,
+      'mouseup',
+      (event: MouseEvent): void => this.onMouseUp(event)
+    );
+  }
 
   onTouchStart(event: TouchEvent): void {
     this.startX = event.touches[0].clientX;
@@ -49,5 +81,12 @@ export class SwipeDirective {
     const deltaY: number = endY - this.startY;
 
     this.swipeEnd.emit({ x: deltaX, y: deltaY });
+  }
+
+  ngOnDestroy(): void {
+    this.touchStartListener();
+    this.touchEndListener();
+    this.mouseDownListener();
+    this.mouseUpListener();
   }
 }
